91e709b6dfac4c8c8db811ef468f439371661e7e
andrews.sato <andrews.sato@gmail.com>
2019-06-29 20:09:48 -0300

    add method to set response with properties

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -1,5 +1,5 @@
 import {
-  observable, action, decorate,
+  observable, action, decorate, computed,
 } from 'mobx';
 import {
   allProperties as AllPropertiesApi,
@@ -10,17 +10,22 @@ class AppStore {
     this.rootStore = rootStore;
     this.title = '...loading...';
     this.allPropertiesApi = AllPropertiesApi;
+    this.properties = [];
   }
 
   setTitle(title) {
     this.title = title;
   }
 
+  setProperties(properties) {
+    this.properties = properties;
+  }
+
   doRequestProperties() {
     const api = async (resolve, reject) => {
       try {
         const res = await this.allPropertiesApi();
-        console.log(res.data);
+        this.setProperties(res.data);
         resolve(res.data);
       } catch (err) {
         reject(err);
@@ -28,11 +33,19 @@ class AppStore {
     };
     return new Promise((resolve, reject) => api(resolve, reject));
   }
+
+  get getAllPropeties() {
+    return this.properties;
+  }
 }
 
 decorate(AppStore, {
+  properties: observable,
   title: observable,
   setTitle: action,
+  doRequestProperties: action,
+  setProperties: action,
+  getAllPropeties: computed,
 });
 
 export default AppStore;
