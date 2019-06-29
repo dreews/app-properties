a5f071c8863ec5c4369f012abc4398f1dc589696
andrews.sato <andrews.sato@gmail.com>
2019-06-29 14:45:36 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+  "globals": {
+    "document": true,
+    "window": true,
+    "describe": true,
+    "it": true,
+    "expect": true,
+    "beforeEach": true
+  },
+  "parser": "babel-eslint"
+};
