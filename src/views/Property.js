f8cbe2155641b4494e08dfa42187ee58eb8bc10e
andrews.sato <andrews.sato@gmail.com>
2019-06-29 20:12:53 -0300

    fix view property with back button

--- src/views/Property.js
+++ src/views/Property.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { Flex, Button, Box } from 'rebass';
+import context from '../stores/context';
 
 class Property extends Component {
-  render () {
+  static contextType = context;
+
+  render() {
+    const { history, match } = this.props;
     return (
-      <div>
-        Property {this.props.match.params.id} {this.props.match.params.portal}
-      </div>
+      <>
+        <Flex p={2} justifyContent="center">
+          <Button
+            bg="black"
+            onClick={() => {
+              history.push(`/portal/${match.params.portal}`);
+            }}
+          >
+            Voltar
+          </Button>
+        </Flex>
+        <Box p={2}>
+          Property {match.params.id} {match.params.portal}
+        </Box>
+      </>
     )
   }
 }
 
-export default Property;
+export default observer(Property);
