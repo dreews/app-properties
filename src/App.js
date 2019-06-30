cababbcd61ad0b81589e377830d04653cda31c5a
andrews.sato <andrews.sato@gmail.com>
2019-06-30 20:29:53 -0300

    add style to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import styled from 'styled-components';
 import { Route } from 'react-router-dom';
 import { Box, Flex, Heading } from 'rebass';
 import Context from './stores/context';
@@ -7,25 +8,40 @@ import Home from './views/Home';
 import Portal from './views/Portal';
 import Property from './views/Property';
 
+const Wrapper = styled(Flex)`
+  min-height: 100vh;
+`;
+
 class App extends Component {
   static contextType = Context;
 
   render() {
     return (
       <div className="App">
-        <Flex
+        <Wrapper
+          flexWrap="wrap"
+          alignItems="center"
           justifyContent="center"
         >
-          <Box p={2}>
-            <Heading color="black" textAlign="center">
-              Teste de engenharia do GrupoZap
-            </Heading>
+          <Box p={[2, 0]} width={[1]}>
+            <Flex justifyContent="center">
+              <Heading
+                color="#333"
+                textAlign="center"
+                fontWeight={[800, 300]}
+                fontSize={[3, 4, 5]}
+                width={[1, 1, 1, 1 / 2]}
+                my={[3, 4, 4]}
+              >
+                Teste de engenharia do GrupoZap
+              </Heading>
+            </Flex>
 
             <Route exact path="/" component={Home} />
             <Route exact path="/portal/:portal" component={Portal} />
             <Route exact path="/portal/:portal/:id" component={Property} />
           </Box>
-        </Flex>
+        </Wrapper>
       </div>
     );
   }
